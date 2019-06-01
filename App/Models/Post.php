71cfb93cd49b2d8d0abb1fc7f327dcb1d21b1d8c
A.D-47 <abdi0361@stud.kea.dk>
2019-06-01 20:32:33 +0200

    update: fix bugs

--- App/Models/Post.php
+++ App/Models/Post.php
@@ -28,6 +28,8 @@ class Post extends \Core\Model
             $stmt = $db->query('SELECT * FROM posts ORDER BY created_at');
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+            
+
             return $results;
             
         } catch (PDOException $e) {
